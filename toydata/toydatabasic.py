01c8fb1fdcd8bb03efb0a298c0c46da56007851c
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-06-22 15:37:05 -0400

    added a simple test EXAMPLE: this is way-too-hard-coded and also too long to be in the module script... better to have a sepaate test script but I gotta run!

--- toydata/toydatabasic.py
+++ toydata/toydatabasic.py
@@ -31,3 +31,26 @@ def _choose_horizontal(x, y, array):
 
 def _choose_vertical(x, y, array):
     array[x:x+5, y] = 180 + array[x:x+5, y]
+
+if __name__ == '__main__':
+
+    import random,sys
+    dims=(7,3) # this is data (2d array) dimension (x,y)
+
+    array=np.zeros(shape=dims).astype(np.float32)
+    for _ in xrange(10000):
+        array = array.reshape(dims)
+        array[:] = 0.
+        x = int(random.random() * (dims[0]-5))
+        y = int(random.random() * dims[1])
+        _choose_vertical(x,y,array)
+        array = array.reshape(dims[0]*dims[1])
+        if not (array>0).sum() == 5:
+            print '# non-zero elements',(array>0).sum(),'(should be 5...)'
+            print array>0
+            sys.exit(1)
+        if not (array[array>0] == 180.).astype(np.int32).sum() == 5:
+            print 'Array contains element not exactly 180...'
+            eys.exit(1)
+    print 'Test successful'
+    sys.exit(0)
