82f6420e16b3456c2c5af9c9aa780f301c70922f
ssfehlberg <ssfehlberg@gmail.comgit config --global user.name ssfehlberggit config --global user.email ssfehlberg@gmail.com>
2017-07-11 16:00:58 -0400

    Fixed the path for loading

--- uboone/ana_pid_classification.py
+++ uboone/ana_pid_classification.py
@@ -76,7 +76,7 @@ saver= tf.train.Saver()
 sess.run(tf.global_variables_initializer())
 #saver= tf.train.Saver()
 save = tf.train.import_meta_graph('%s.meta' % cfg.ANA_FILE)
-save.restore(sess,tf.train.latest_checkpoint('/data/ssfehlberg/toymodel/'))
+save.restore(sess,tf.train.latest_checkpoint('./'))
 
 #GOOD FOR DEBUGGIN'!!!!!
 #for var in tf.trainable_variables():
