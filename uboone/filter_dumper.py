b4a3bf74bc6ee24270b7642f17a5770bcafc423e
ssfehlberg <ssfehlberg@gmail.comgit config --global user.name ssfehlberggit config --global user.email ssfehlberg@gmail.com>
2017-09-27 16:15:02 -0400

    working!

--- uboone/filter_dumper.py
+++ uboone/filter_dumper.py
@@ -49,7 +49,6 @@ if zoom:
 img.write_png('Entry%05d.png' % entry)
 plt.close()
 
-
 ##################################
 #Need this stuff in order to restore tf session
 
@@ -68,7 +67,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from dataloader import larcv_data
-                                                                                                                                                                                                                                                                                                   
+
 # main part starts here #                                                                                                                                                                                                                                                                                                   
   
 # Step 0: configure IO                                                                                                                                                                                                                                                                                                     
@@ -101,23 +100,23 @@ proc.read_next(cfg.BATCH_SIZE)
 #Functions to get out the filters
 
 def getActivations(layer,stimuli):
-    units = sess.run(layer,feed_dict={data_tensor:np.reshape(stimuli,[1,262144]),keep_prob:1.0})
-    units = np.expand_dims(units, axis=3)
+    units = sess.run(layer,feed_dict={x_1:np.reshape(stimuli,[1,262144]),keep_prob:1.0})
     plotNNFilter(units)
 
 def plotNNFilter(units):
-    filters = units.shape[2]
-   # filters = np.expand_dims(filters,axis =0)
-    plt.figure(1, figsize=(100,100))
+    filters = units.shape[3]
+    my_dpi=72
+    #filters = np.expand_dims(filters,axis =0)
+    plt.figure(1, figsize=(745/my_dpi, 513/my_dpi), dpi=my_dpi)
     n_columns = 6
     n_rows = math.ceil(filters / n_columns) + 1
     for i in range(filters):
-        fig,ax=plt.subplots(figsize=(12,8),facecolor='w')
-        plt.subplot(n_rows, n_columns, i+1)
+        #fig,ax=plt.subplots(figsize=(12,8),facecolor='w')
+        #plt.subplot(n_rows, n_columns, i+1)
         plt.title('Filter ' + str(i))
         #Problem is arising here from image dimension and between filters shape[3]
-        img=plt.imshow(units[:,:,i], interpolation="nearest", cmap="jet")
-        img.write_png('Filters_Entry%05d.png' % entry)
+        img=plt.imshow(units[0,:,:,i], interpolation="nearest", cmap="jet")
+        img.write_png('Filters_Entry'+str(entry)+'_'+str(i)+'.png')
         plt.close()
 
 ########################
@@ -130,19 +129,43 @@ sess.run(tf.global_variables_initializer())
 saver= tf.train.import_meta_graph('/data/drinkingkazu/summer2017/toymodel/train_multiclass2/pid-18199.meta')
 saver.restore(sess,'/data/drinkingkazu/summer2017/toymodel/train_multiclass2/pid-18199')
 graph = tf.get_default_graph()
-  
+x_1 = graph.get_tensor_by_name("x_1:0") 
+ 
+ 
 #Get out the names of everything now to find the name of the filter you want
 #print(sess.graph.get_operations())
  
 # How to access saved operation
-op_to_restore = graph.get_tensor_by_name("fc_final/fc_final_weights/RMSProp_1:0")
+op_to_restore11 = graph.get_tensor_by_name("conv1_1/Relu:0")
+#op_to_restore12 = graph.get_tensor_by_name("conv1_2/conv1_2_weights/RMSProp_1:0")
+#op_to_restore21 = graph.get_tensor_by_name("conv2_1/conv2_1_weights/RMSProp_1:0")
+#op_to_restore22 = graph.get_tensor_by_name("conv2_2/conv2_2_weights/RMSProp_1:0")
+#op_to_restore31 = graph.get_tensor_by_name("conv3_1/conv3_1_weights/RMSProp_1:0")
+#op_to_restore32 = graph.get_tensor_by_name("conv3_2/conv3_2_weights/RMSProp_1:0")
+#op_to_restore41 = graph.get_tensor_by_name("conv4_1/conv4_1_weights/RMSProp_1:0")
+#op_to_restore42 = graph.get_tensor_by_name("conv4_2/conv4_2_weights/RMSProp_1:0")
+#op_to_restore51 = graph.get_tensor_by_name("conv5_1/conv5_1_weights/RMSProp_1:0")
+#op_to_restore52 = graph.get_tensor_by_name("conv5_2/conv5_2_weights/RMSProp_1:0")
+#op_to_restore = graph.get_tensor_by_name("fc_final/fc_final_weights/RMSProp_1:0")
+
 #Use below print step for debugging to make sure you are restoring a tensor
-#print(op_to_restore)
+#print(op_to_restore11)
 
 #########################
 #The magic filter step!
 imageToUse= adcimg
-getActivations(op_to_restore,adcimg)
+getActivations(op_to_restore11,adcimg)
+#getActivations(op_to_restore12,adcimg)
+#getActivations(op_to_restore21,adcimg)
+#getActivations(op_to_restore22,adcimg)
+#getActivations(op_to_restore31,adcimg)
+#getActivations(op_to_restore32,adcimg)
+#getActivations(op_to_restore41,adcimg)
+#getActivations(op_to_restore42,adcimg)
+#getActivations(op_to_restore51,adcimg)
+#getActivations(op_to_restore52,adcimg)
+#getActivations(op_to_restore,adcimg)
+
 
 
 
